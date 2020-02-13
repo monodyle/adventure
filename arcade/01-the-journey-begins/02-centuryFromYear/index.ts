e7fb4e435d003bc9c141b518c436fcedf99d0235
root <monodylh@gmail.com>
2020-02-14 03:08:06 +0700

    folder structure

--- arcade/01-the-journey-begins/02-centuryFromYear/index.ts
+++
@@ -1,8 +0,0 @@
-function centuryFromYear(year: number): number {
-  let d: number = year % 100
-  let c: number = Math.floor(year / 100)
-  if (d == 0) return c
-  else return c + 1
-}
-
-export { centuryFromYear }
--- arcade/01-the-journey-begins/03-checkPalindrome/index.ts
+++
@@ -1,5 +0,0 @@
-function checkPalindrome(inputString: string): boolean {
-  return inputString === inputString.split('').reverse().join('');
-}
-
-export {checkPalindrome}
