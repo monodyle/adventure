5af26428e56ee195894970abc2f5e15e25f17aa7
root <monodylh@gmail.com>
2020-02-14 15:50:28 +0700

    level 3 / 12

--- codesignal/01-arcade/03-smooth-sailing/12-sortByHeight/index.ts
+++ codesignal/01-arcade/03-smooth-sailing/12-sortByHeight/index.ts
@@ -8,7 +8,7 @@
 function sortByHeight(a: number[]): number[] {
   const normalSort: Array<number> = a.filter(i => i !== -1).sort((a, b) => a - b)
   return a.map(i => {
-    return (i !== -1) normalSort.shift() : -1
+    return (i !== -1) ? normalSort.shift() : -1
   })
 }
 
