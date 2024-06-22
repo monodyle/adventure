d8c2cb082b89c9be1eebfe253786edfb1ed7f8ec
monodyle <monodylh@gmail.com>
2024-06-22 18:24:41 +0700

    problem 1358

---
+++ leetcode/1358-number-of-substrings-containing-all-three-characters/README.md
@@ -0,0 +1,3 @@
+## 1358. Number of Substrings Containing All Three Characters
+
+Problem: https://leetcode.com/problems/number-of-substrings-containing-all-three-characters/description/
---
+++ leetcode/1358-number-of-substrings-containing-all-three-characters/src/lib.rs
@@ -0,0 +1,40 @@
+pub fn number_of_substrings(s: String) -> i32 {
+    let s = s.as_bytes();
+    let mut ans = 0;
+    let mut cnt = vec![0; 3];
+
+    let mut cur = 0;
+    for &char in s {
+        cnt[(char - b'a') as usize] += 1;
+        while cnt.iter().all(|&c| c > 0) {
+            cnt[(s[cur] - b'a') as usize] -= 1;
+            cur += 1;
+        }
+        ans += cur;
+    }
+
+    ans as i32
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn example_1() {
+        let result = number_of_substrings("abcabc".to_string());
+        assert_eq!(result, 10);
+    }
+
+    #[test]
+    fn example_2() {
+        let result = number_of_substrings("aaacb".to_string());
+        assert_eq!(result, 3);
+    }
+
+    #[test]
+    fn example_3() {
+        let result = number_of_substrings("abc".to_string());
+        assert_eq!(result, 1);
+    }
+}
