12a4b894368108bed0721606431f9938d4c4ffd2
Monody Le <30283022+monodyle@users.noreply.github.com>
2025-01-07 11:51:55 +0700

    selection sort

---
+++ generic/sort/selection-sort.js
@@ -0,0 +1,18 @@
+/**
+ * @param {Array<number>} arr The input integer array to be sorted.
+ * @return {Array<number>}
+ */
+export default function selectionSort(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    let min = i
+    for (let j = i+1; j < arr.length; j++) {
+      if (arr[j] < arr[min]) {
+        min = j
+      }
+    }
+    if (min !== i) {
+      [arr[min], arr[i]] = [arr[i], arr[min]]
+    }
+  }
+    return arr
+}
---
+++ generic/sort/selection-sort.test.js
@@ -0,0 +1,40 @@
+import { expect, test, describe } from "vitest";
+import selectionSort from "./selection-sort";
+
+describe("selectionSort", () => {
+	test("empty", () => {
+		expect(selectionSort([])).toEqual([]);
+	});
+
+	test("one element", () => {
+		expect(selectionSort([1])).toEqual([1]);
+	});
+
+	test("two elements", () => {
+		expect(selectionSort([2, 1])).toEqual([1, 2]);
+		expect(selectionSort([1, 2])).toEqual([1, 2]);
+	});
+
+	test("more than two elements", () => {
+		expect(selectionSort([10, 2, 4])).toEqual([2, 4, 10]);
+		expect(selectionSort([4, 5, 6, 1, 2, 3])).toEqual([1, 2, 3, 4, 5, 6]);
+		expect(selectionSort([1, 2, 3, 4, 5, 0])).toEqual([0, 1, 2, 3, 4, 5]);
+		expect(selectionSort([10, 9, 8, 7, 6, 5, 4, 3, 2, 1])).toEqual([
+			1, 2, 3, 4, 5, 6, 7, 8, 9, 10,
+		]);
+		expect(selectionSort([5, 4, 3, 2, 1, 10, 9, 8, 7, 6])).toEqual([
+			1, 2, 3, 4, 5, 6, 7, 8, 9, 10,
+		]);
+		expect(selectionSort([98322, 3242, 876, -234, 34, 12331])).toEqual([
+			-234, 34, 876, 3242, 12331, 98322,
+		]);
+	});
+
+	test("duplicate elements", () => {
+		expect(selectionSort([1, 1])).toEqual([1, 1]);
+		expect(selectionSort([2, 2, 2])).toEqual([2, 2, 2]);
+		expect(selectionSort([2, 1, 2])).toEqual([1, 2, 2]);
+		expect(selectionSort([1, 1, 1, 1, 1, 1])).toEqual([1, 1, 1, 1, 1, 1]);
+		expect(selectionSort([7, 2, 4, 3, 1, 2])).toEqual([1, 2, 2, 3, 4, 7]);
+	});
+});
