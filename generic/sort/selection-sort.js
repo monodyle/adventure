d1fcc0058d5e3cb53d54ce58f19145776c7e1184
Monody Le <30283022+monodyle@users.noreply.github.com>
2025-01-07 13:44:56 +0700

    format

--- generic/sort/selection-sort.js
+++ generic/sort/selection-sort.js
@@ -3,16 +3,16 @@
  * @return {Array<number>}
  */
 export default function selectionSort(arr) {
-  for (let i = 0; i < arr.length; i++) {
-    let min = i
-    for (let j = i+1; j < arr.length; j++) {
-      if (arr[j] < arr[min]) {
-        min = j
-      }
-    }
-    if (min !== i) {
-      [arr[min], arr[i]] = [arr[i], arr[min]]
-    }
-  }
-    return arr
+	for (let i = 0; i < arr.length; i++) {
+		let min = i;
+		for (let j = i + 1; j < arr.length; j++) {
+			if (arr[j] < arr[min]) {
+				min = j;
+			}
+		}
+		if (min !== i) {
+			[arr[min], arr[i]] = [arr[i], arr[min]];
+		}
+	}
+	return arr;
 }
