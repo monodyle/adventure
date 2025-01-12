abb08b1ad3ce6c9ecb5aa8f64faad9df901d1cb1
monodyle <monodylh@gmail.com>
2025-01-12 15:15:30 +0700

    rename import name

--- generic/sort/heap-sort.test.js
+++ generic/sort/heap-sort.test.js
@@ -1,40 +1,40 @@
 import { expect, test, describe } from "vitest";
-import heapSort from "./heap-sort";
+import sort from "./heap-sort";
 
 describe("heapSort", () => {
 	test("empty", () => {
-		expect(heapSort([])).toEqual([]);
+		expect(sort([])).toEqual([]);
 	});
 
 	test("one element", () => {
-		expect(heapSort([1])).toEqual([1]);
+		expect(sort([1])).toEqual([1]);
 	});
 
 	test("two elements", () => {
-		expect(heapSort([2, 1])).toEqual([1, 2]);
-		expect(heapSort([1, 2])).toEqual([1, 2]);
+		expect(sort([2, 1])).toEqual([1, 2]);
+		expect(sort([1, 2])).toEqual([1, 2]);
 	});
 
 	test("more than two elements", () => {
-		expect(heapSort([10, 2, 4])).toEqual([2, 4, 10]);
-		expect(heapSort([4, 5, 6, 1, 2, 3])).toEqual([1, 2, 3, 4, 5, 6]);
-		expect(heapSort([1, 2, 3, 4, 5, 0])).toEqual([0, 1, 2, 3, 4, 5]);
-		expect(heapSort([10, 9, 8, 7, 6, 5, 4, 3, 2, 1])).toEqual([
+		expect(sort([10, 2, 4])).toEqual([2, 4, 10]);
+		expect(sort([4, 5, 6, 1, 2, 3])).toEqual([1, 2, 3, 4, 5, 6]);
+		expect(sort([1, 2, 3, 4, 5, 0])).toEqual([0, 1, 2, 3, 4, 5]);
+		expect(sort([10, 9, 8, 7, 6, 5, 4, 3, 2, 1])).toEqual([
 			1, 2, 3, 4, 5, 6, 7, 8, 9, 10,
 		]);
-		expect(heapSort([5, 4, 3, 2, 1, 10, 9, 8, 7, 6])).toEqual([
+		expect(sort([5, 4, 3, 2, 1, 10, 9, 8, 7, 6])).toEqual([
 			1, 2, 3, 4, 5, 6, 7, 8, 9, 10,
 		]);
-		expect(heapSort([98322, 3242, 876, -234, 34, 12331])).toEqual([
+		expect(sort([98322, 3242, 876, -234, 34, 12331])).toEqual([
 			-234, 34, 876, 3242, 12331, 98322,
 		]);
 	});
 
 	test("duplicate elements", () => {
-		expect(heapSort([1, 1])).toEqual([1, 1]);
-		expect(heapSort([2, 2, 2])).toEqual([2, 2, 2]);
-		expect(heapSort([2, 1, 2])).toEqual([1, 2, 2]);
-		expect(heapSort([1, 1, 1, 1, 1, 1])).toEqual([1, 1, 1, 1, 1, 1]);
-		expect(heapSort([7, 2, 4, 3, 1, 2])).toEqual([1, 2, 2, 3, 4, 7]);
+		expect(sort([1, 1])).toEqual([1, 1]);
+		expect(sort([2, 2, 2])).toEqual([2, 2, 2]);
+		expect(sort([2, 1, 2])).toEqual([1, 2, 2]);
+		expect(sort([1, 1, 1, 1, 1, 1])).toEqual([1, 1, 1, 1, 1, 1]);
+		expect(sort([7, 2, 4, 3, 1, 2])).toEqual([1, 2, 2, 3, 4, 7]);
 	});
 });
--- generic/sort/quick-sort.test.js
+++ generic/sort/quick-sort.test.js
@@ -1,40 +1,40 @@
 import { expect, test, describe } from "vitest";
-import quickSort from "./quick-sort";
+import sort from "./quick-sort";
 
 describe("quickSort", () => {
 	test("empty", () => {
-		expect(quickSort([])).toEqual([]);
+		expect(sort([])).toEqual([]);
 	});
 
 	test("one element", () => {
-		expect(quickSort([1])).toEqual([1]);
+		expect(sort([1])).toEqual([1]);
 	});
 
 	test("two elements", () => {
-		expect(quickSort([2, 1])).toEqual([1, 2]);
-		expect(quickSort([1, 2])).toEqual([1, 2]);
+		expect(sort([2, 1])).toEqual([1, 2]);
+		expect(sort([1, 2])).toEqual([1, 2]);
 	});
 
 	test("more than two elements", () => {
-		expect(quickSort([10, 2, 4])).toEqual([2, 4, 10]);
-		expect(quickSort([4, 5, 6, 1, 2, 3])).toEqual([1, 2, 3, 4, 5, 6]);
-		expect(quickSort([1, 2, 3, 4, 5, 0])).toEqual([0, 1, 2, 3, 4, 5]);
-		expect(quickSort([10, 9, 8, 7, 6, 5, 4, 3, 2, 1])).toEqual([
+		expect(sort([10, 2, 4])).toEqual([2, 4, 10]);
+		expect(sort([4, 5, 6, 1, 2, 3])).toEqual([1, 2, 3, 4, 5, 6]);
+		expect(sort([1, 2, 3, 4, 5, 0])).toEqual([0, 1, 2, 3, 4, 5]);
+		expect(sort([10, 9, 8, 7, 6, 5, 4, 3, 2, 1])).toEqual([
 			1, 2, 3, 4, 5, 6, 7, 8, 9, 10,
 		]);
-		expect(quickSort([5, 4, 3, 2, 1, 10, 9, 8, 7, 6])).toEqual([
+		expect(sort([5, 4, 3, 2, 1, 10, 9, 8, 7, 6])).toEqual([
 			1, 2, 3, 4, 5, 6, 7, 8, 9, 10,
 		]);
-		expect(quickSort([98322, 3242, 876, -234, 34, 12331])).toEqual([
+		expect(sort([98322, 3242, 876, -234, 34, 12331])).toEqual([
 			-234, 34, 876, 3242, 12331, 98322,
 		]);
 	});
 
 	test("duplicate elements", () => {
-		expect(quickSort([1, 1])).toEqual([1, 1]);
-		expect(quickSort([2, 2, 2])).toEqual([2, 2, 2]);
-		expect(quickSort([2, 1, 2])).toEqual([1, 2, 2]);
-		expect(quickSort([1, 1, 1, 1, 1, 1])).toEqual([1, 1, 1, 1, 1, 1]);
-		expect(quickSort([7, 2, 4, 3, 1, 2])).toEqual([1, 2, 2, 3, 4, 7]);
+		expect(sort([1, 1])).toEqual([1, 1]);
+		expect(sort([2, 2, 2])).toEqual([2, 2, 2]);
+		expect(sort([2, 1, 2])).toEqual([1, 2, 2]);
+		expect(sort([1, 1, 1, 1, 1, 1])).toEqual([1, 1, 1, 1, 1, 1]);
+		expect(sort([7, 2, 4, 3, 1, 2])).toEqual([1, 2, 2, 3, 4, 7]);
 	});
 });
